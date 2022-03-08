772d2e8b11b36c5687dcae3860ca447d4a80a4bc
Lucas Nascimento Lopes <85919181+lucasnacimento@users.noreply.github.com>
2022-03-08 14:41:25 -0300

    NavBar

---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as GithubIcon } from "/projetos/dsmove/frontend/src/assets/img/github.svg";
+import './styles.css';
+
+function NavBar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="https://github.com/lucasnacimento">
+            <div className="dsmovie-contect-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/lucasnacimento</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default NavBar;
