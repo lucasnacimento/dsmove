7eed617627354eaed38ec8e96b1be9ce2bbe6982
Lucas Nascimento Lopes <85919181+lucasnacimento@users.noreply.github.com>
2022-03-09 13:19:58 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/sds/dsmove/entities/Score.java
@@ -0,0 +1,29 @@
+package com.sds.dsmove.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_score")
+public class Score {
+    
+    @EmbeddedId
+    private ScorePK id = new ScorePK();
+    private Double value;
+
+    public void setMovie(Movie movie) {
+        id.setMovie(movie);
+    }
+
+    public void setUser(User user) {
+        id.setUser(user);
+    }
+
+
+}
---
+++ backend/src/main/java/com/sds/dsmove/entities/ScorePK.java
@@ -0,0 +1,25 @@
+package com.sds.dsmove.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Embeddable
+public class ScorePK implements Serializable{
+
+    @ManyToOne
+    @JoinColumn(name="movie_id")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+}
