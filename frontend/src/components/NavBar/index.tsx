a08a6c1f3f9b9d5d6984afd5612554258a42bc2b
Lucas Nascimento Lopes <85919181+lucasnacimento@users.noreply.github.com>
2022-03-11 19:36:58 -0300

    nova correcao

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import { ReactComponent as GithubIcon } from "/projetos/dsmovie/frontend/src/assets/img/github.svg";
+import { ReactComponent as GithubIcon } from "../../assets/img/github.svg";
 import './styles.css';
 
 function NavBar() {
